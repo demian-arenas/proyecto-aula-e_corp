51d644be3a1ea063ca5d734d09cefadf8fee7ca9
Demian Arenas <arenasdemian99@gmail.com>
2017-05-19 07:40:53 -0500

    Creacion de la funcion para buscar ciudades

--- prueba.js
+++ prueba.js
@@ -14,24 +14,5 @@ let dataUsuario = {
   idOrigen: 1,
   idTipoUsuario: 1
 }
-let queryString = ' INSER INTO `e_corp`.`musuarioaut` (`nck_usr`, `pass_usr`, `id_udt`, `id_tps`)' +
-      ' VALUES ("' + dataUsuario.nickname + '","' + dataUsuario.pass + '",' + dataUsuario.idDatos + ',' + dataUsuario.idTipoUsuario + ');'
-console.log(queryString)
 
-db.saveUserData(dataUsuario)
-  .then((rows) => {
-    console.log(rows)
-    let idDatos = rows.insertId
-    dataUsuario.idDatos = idDatos
-    console.log(idDatos)
-    console.log(dataUsuario.idDatos)
-    queryString = ' INSER INTO `e_corp`.`musuarioaut` (`nck_usr`, `pass_usr`, `id_udt`, `id_tps`)' +
-      ' VALUES ("' + dataUsuario.nickname + '","' + dataUsuario.pass + '",' + dataUsuario.idDatos + ',' + dataUsuario.idTipoUsuario + ');'
-    console.log(queryString)
-    db.saveUserAut(dataUsuario).then((rows) => {
-      console.log(rows)
-    })
-    .catch((err) => console.log(err))
-  })
-  .catch((err) => console.log(err))
-  
+db.getHeroes(dataUsuario.nickname).then((rows) => console.log(rows)).catch((err) => console.log(err))
